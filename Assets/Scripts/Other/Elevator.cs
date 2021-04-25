2ece0156e463f7cdc8b9bb2793a911b064502b1e
Franciszek Pyrć <franciszekpyrc@gmail.com>
2021-04-25 13:09:29 +0200

    Added elevators

---
+++ Assets/Scripts/Other/Elevator.cs
@@ -0,0 +1,60 @@
+using qASIC;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine.Events;
+
+public class Elevator : MonoBehaviour
+{
+	public TriggerController controller;
+	public bool isEnd;
+    public string sceneName;
+
+    [Header("Animation")]
+    private Animator anim;
+    public string DoorCloseTriggerName = "close";
+    public float SceneLoadDelay = 5f;
+    public string DoorOpenTriggerName = "open";
+    public float DoorOpenDelay = 3f;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        controller?.ColliderEnter?.AddListener(OnInElevator);
+    }
+
+    private void Start()
+    {
+        if (isEnd)
+        {
+            anim.SetTrigger(DoorOpenTriggerName);
+            return;
+        }
+
+        StartCoroutine(InvokeActionAfterDelay(() => anim.SetTrigger(DoorOpenTriggerName), DoorOpenDelay));
+    }
+
+    public void OnInElevator(Collider collider)
+    {
+        if (!isEnd) return;
+        anim?.SetTrigger(DoorCloseTriggerName);
+        StartCoroutine(InvokeActionAfterDelay(LoadScene, SceneLoadDelay));
+    }
+
+    public void LoadScene()
+    {
+        if (!Application.CanStreamedLevelBeLoaded(sceneName))
+        {
+            qDebug.LogError("Cannot load scene in elevator!");
+            return;
+        }
+
+        SceneManager.LoadScene(sceneName);
+    }
+
+    IEnumerator InvokeActionAfterDelay(UnityAction action, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        action.Invoke();
+    }
+}
