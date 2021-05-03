0a60af81664a9a1b70fc54aca29730c53a1f823e
Franciszek Pyrć <franciszekpyrc@gmail.com>
2021-05-03 17:02:11 +0200

    Fixed AI on level 3

--- Assets/Scripts/Enemies/FragmentController.cs
+++ Assets/Scripts/Enemies/FragmentController.cs
@@ -94,7 +94,7 @@ public class FragmentController : MonoBehaviour
 
     public void SpotPlayer()
     {
-        if (state == FragmentState.chasing) return;
+        if (state != FragmentState.idle) return;
         rb.velocity = new Vector3(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
         state = FragmentState.chasing;
     }
