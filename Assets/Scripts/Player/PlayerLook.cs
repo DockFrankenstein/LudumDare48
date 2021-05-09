9da61eda15e5211927af7b104bc7950bac274409
Franciszek Pyrć <franciszekpyrc@gmail.com>
2021-05-09 18:20:18 +0200

    Fixed issues with level5 and mouse sensitivity

--- Assets/Scripts/Player/PlayerLook.cs
+++ Assets/Scripts/Player/PlayerLook.cs
@@ -5,6 +5,7 @@ public class PlayerLook : MonoBehaviour
     public Transform Axis;
 
     public float MouseSensitivity = 1f;
+    public float WebSensitivity = 1f;
 
     private void Update()
     {
@@ -33,8 +34,9 @@ public class PlayerLook : MonoBehaviour
 
     private void Rotate()
     {
-        rotation.x = rotation.x + Input.GetAxis("Mouse X") * MouseSensitivity;
-        rotation.y = Mathf.Clamp(rotation.y - Input.GetAxis("Mouse Y") * MouseSensitivity, -90f, 90f);
+        float sens = Application.platform == RuntimePlatform.WebGLPlayer ? WebSensitivity : MouseSensitivity;
+        rotation.x = rotation.x + Input.GetAxis("Mouse X") * sens;
+        rotation.y = Mathf.Clamp(rotation.y - Input.GetAxis("Mouse Y") * sens, -90f, 90f);
         transform.eulerAngles = new Vector3(transform.eulerAngles.x, rotation.x, transform.eulerAngles.z);
         if (Axis != null) Axis.eulerAngles = new Vector3(rotation.y, Axis.eulerAngles.y, Axis.eulerAngles.z);
     }
